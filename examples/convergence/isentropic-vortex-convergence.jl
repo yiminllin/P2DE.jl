a380d113c579671505cf2f901ddc0b72ca7b7003
Yimin Lin <yiminlllin@gmail.com>
2024-06-02 22:02:28 -0700

    Dump

--- examples/convergence/isentropic-vortex-convergence.jl
+++ examples/convergence/isentropic-vortex-convergence.jl
@@ -1,7 +1,7 @@
 using Revise
 using StaticArrays
-using DataFrames
-using JLD2
+# using DataFrames
+# using JLD2
 using StartUpDG
 
 using P2DE
@@ -44,26 +44,28 @@ function initial_condition(param, x, y)
     return primitive_to_conservative(param.equation, SVector(exact_sol(param.equation, x, y, t0)))
 end
 
-# TODO: refactor convergence
-jld_path = "/data/yl184/outputs/jld2/isentropic-vortex.jld2"
+# # TODO: refactor convergence
+# jld_path = "/home/yiminlin/outputs/jld2/isentropic-vortex.jld2"
+#
 
-for limiter_type in [(NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), ZhangShuLimiter(), LobattoCollocation());
+for limiter_type in [
+    (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), ZhangShuLimiter(), LobattoCollocation());
     (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityBound()), LobattoCollocation());
+    (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityBound(), shockcapture_type=HennemannShockCapture()), LobattoCollocation());
     (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityAndMinEntropyBound()), LobattoCollocation());
-    (NodewiseScaledExtrapolation(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityBound()), GaussCollocation());
-    (NodewiseScaledExtrapolation(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityAndMinEntropyBound()), GaussCollocation());
-    (NodewiseScaledExtrapolation(), LaxFriedrichsOnProjectedVal(), ZhangShuLimiter(), GaussCollocation())]
+    (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityAndRelaxedMinEntropyBound()), LobattoCollocation());
+    (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityAndCellEntropyBound()), LobattoCollocation());
+    (NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), SubcellLimiter(bound_type=PositivityAndRelaxedCellEntropyBound(beta=0.5)), LobattoCollocation())
+]
     for N in [1; 2; 3; 4]
-        for K in [(5, 5), (10, 10), (20, 20), (40, 40), (80, 80)]
-
-
+        for K in [(5, 5)]
             entropyproj_type, low_order_flux_type, rhs_lim_type, discretization_type = limiter_type
             γ = 1.4
             param = Param(N=N, K=K, xL=(0.0, 0.0), xR=(10.0, 10.0),
                 global_constants=GlobalConstant(POSTOL=1e-14, ZEROTOL=5e-16),
-                timestepping_param=TimesteppingParameter(T=1.0, CFL=0.75, dt0=1e-2, t0=0.0),
+                timestepping_param=TimesteppingParameter(T=0.1, CFL=0.75, dt0=1e-2, t0=0.0),
                 limiting_param=LimitingParameter(ζ=0.1, η=0.5),
-                postprocessing_param=PostprocessingParameter(output_interval=100),
+                postprocessing_param=PostprocessingParameter(output_interval=10000),
                 equation=CompressibleEulerIdealGas{Dim2}(γ),
                 rhs_type=ESLimitedLowOrderPos(low_order_surface_flux_type=low_order_flux_type,
                     high_order_surface_flux_type=LaxFriedrichsOnProjectedVal()),
@@ -82,12 +84,12 @@ for limiter_type in [(NoEntropyProjectionLimiter(), LaxFriedrichsOnNodalVal(), Z
 
             err_data = calculate_error(prealloc.Uq, param, discrete_data, md, prealloc, exact_sol)
 
-            df = DataFrame([name => [] for name in (fieldnames(Param)..., fieldnames(ErrorData)..., :data_history)])
-            write_to_jld2(param, data_hist, err_data, df, jld_path)
+            # df = DataFrame([name => [] for name in (fieldnames(Param)..., fieldnames(ErrorData)..., :data_history)])
+            # write_to_jld2(param, data_hist, err_data, df, jld_path)
 
         end
     end
 end
 
-df = load(jld_path, "data")
-visualize_error_data(df)
+# df = load(jld_path, "data")
+# visualize_error_data(df)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "P2DE.jl" begin
     # Write your tests here.
+    @time begin
+        @test true
+        run(`$(Base.julia_cmd()) --threads=2 $(abspath("../examples/convergence/isentropic-vortex-convergence.jl"))`)
+    end
 end
