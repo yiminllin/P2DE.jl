89b16b42d3a5da4b68a07f5aaf44515f5a30cf7d
yimin lin <yiminlllin@gmail.com>
2024-06-04 06:22:07 -0700

    [Bug] [Infra] Fix Index Bug in Subcell Limiter, Add Smoke Test in CI (#101)
    * Dump
    
    * Dump
    
    * Dump
    
    * Fix index bug in subcell limiter
    
    * Revert isentropic vortex
    
    * Add smoke tests
    
    * Remove unused dependency
    
    * Remove Manifest.toml
    
    * Rename

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,16 @@ using P2DE
 using Test
 
 @testset "P2DE.jl" begin
-    # Write your tests here.
+    @testset "Smoke Test - Single Thread" begin
+        @time begin
+            @test true
+            run(`$(Base.julia_cmd()) --threads=1 $(abspath("test_smoke.jl"))`)
+        end
+    end
+    @testset "Smoke Test - Single Thread" begin
+        @time begin
+            @test true
+            run(`$(Base.julia_cmd()) --threads=2 $(abspath("test_smoke.jl"))`)
+        end
+    end
 end
