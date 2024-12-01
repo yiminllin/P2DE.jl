040ba0269a8e2a66cc506b46c8890dd42efa06b3
Yimin Lin <yiminlllin@gmail.com>
2024-11-30 22:02:45 -0800

    Refactor neighbor index calculation

--- src/dg/utils.jl
+++ src/dg/utils.jl
@@ -1,3 +1,23 @@
+"""
+    neighbor_index(i, Nfp)
+
+Compute the neighboring face (node, element) index `(iP, kP)` of the face node with global face node index `i`.
+"""
+function neighbor_index(i, Nfp)
+    iP = mod1(i, Nfp)
+    kP = div(i - 1, Nfp) + 1
+    return iP, kP
+end
+
+"""
+    neighbor_index(i, k, Nfp, mapP)
+
+Given the face map `mapP`, of size `Nfp`x`K`, compute the neighboring face (node, element) index `(iP, kP)` of the face node with node index `i` and element index `k`.
+"""
+function neighbor_index(i, k, Nfp, mapP)
+    return neighbor_index(mapP[i, k], Nfp)
+end
+
 function check_conservation(state, solver)
     (; Uq) = state.preallocation
     (; K, Nq) = solver.discrete_data.sizes
